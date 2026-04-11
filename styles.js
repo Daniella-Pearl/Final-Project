4e810ac7b46908083c0628b58d02858811e6d3ac
Nassuuna Pearl Daniella <pearldaniellu9@gmail.com>
2026-04-11 23:09:54 +0300

    landingpage and inventory

--- styles.js
+++ styles.js
@@ -2,3 +2,10 @@
 document.querySelector(".menu-btn").onclick = () => {
   document.querySelector(".sidebar").classList.toggle("active");
 };
+
+var typed = new typed('#typed', {
+  strings: ['Nails.', 'Iron Bars.', 'Cement.', 'Paint.'],
+  typeSpeed: 50,
+  backSpeed: 30,
+  loop: true
+});
